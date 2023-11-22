a12de75308a59218461240b3cd81c9289b3f628a
vitormoscoso <vitormoscoso@gmail.com>
2023-11-22 20:35:12 -0300

    adding logic

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,133 @@
+import { useState, useEffect } from "react";
 import useAxios from "../../utils/apiClient";
-
 export default function Home() {
-  const { data, loading, error } = useAxios();
+  const [answer, setAnswer] = useState("");
+  const [buttonColors, setButtonColors] = useState<{ [key: string]: string }>(
+    {}
+  );
+  const [showText, setShowText] = useState(false);
+  const [attempt, setAttempt] = useState(0);
+  const [points, setPoints] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  console.log(data);
-  
+  const { data, options, text, loading } = useAxios(currentPage);
+
+  useEffect(() => {
+    if (data !== undefined) setAnswer(data?.[0]?.name?.common);
+  }, [data]);
+
+  const handleClick = (name: string) => {
+    setAttempt(1);
+    const newColors = { ...buttonColors };
+    newColors[name] = name === answer ? "limegreen" : "orangered";
+    setButtonColors(newColors);
+    if (name === answer) {
+      setShowText(true);
+      setPoints(points + 1);
+    } else {
+      setShowText(false);
+    }
+  };
+  const handleClickRestart = () => {
+    setCurrentPage(1);
+    setAttempt(0);
+    setButtonColors({});
+    setShowText(false);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+    setAttempt(0);
+    setButtonColors({});
+    setShowText(false);
+  };
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "whitesmoke",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "20%",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <p>{currentPage}/15</p>
+          {currentPage >= 15 ? (
+            <button onClick={handleClickRestart}>Reiniciar</button>
+          ) : (
+            <button onClick={handleNextPage}>Próxima</button>
+          )}
+          {/* {attempt === 1 ? (
+            <button
+              style={{ height: "fit-content" }}
+              onClick={() => {
+                setAttempt(0);
+                setButtonColors({});
+                setShowText(false);
+              }}
+            >
+              Outra tentativa
+            </button>
+          ) : (
+            <></>
+          )} */}
+        </div>
+        {loading ? (
+          <></>
+        ) : (
+          <img
+            alt="flag"
+            src={data?.[0]?.flags?.png}
+            style={{
+              minHeight: "25vh",
+              maxHeight: "25vh",
+              minWidth: "20vw",
+              maxWidth: "20vw",
+            }}
+          ></img>
+        )}
+        {options?.map((countryName) => (
+          <button
+            key={countryName}
+            style={{
+              border: "1px solid",
+              width: "100%",
+              textAlign: "center",
+              marginTop: "5px",
+              marginBottom: "5px",
+              padding: "5px",
+              backgroundColor: buttonColors[countryName] || "",
+              cursor: `${attempt === 1 ? "default" : "pointer"}`,
+            }}
+            onClick={() => handleClick(countryName)}
+            disabled={attempt === 1 ? true : false}
+          >
+            {countryName}
+          </button>
+        ))}
+        {showText ? <p style={{ maxWidth: "100%" }}>{text}</p> : <></>}
+      </div>
     </div>
   );
 }
--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,44 +1,100 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+function shuffleArray(array: string[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Troca os elementos
+  }
+  return array;
+}
+
+function getText(
+  country: string,
+  area: number,
+  population: number,
+  capital: string,
+  continents: string
+): string {
+  const formattedArea = new Intl.NumberFormat("pt-BR", {
+    style: "decimal",
+  }).format(area);
+  const formattedPop = new Intl.NumberFormat("pt-BR", {
+    style: "decimal",
+  }).format(population);
+
+  return `${country} possui uma área de ${formattedArea} km², com uma população de ${formattedPop}. A capital é ${capital} e pertence ao continente da ${continents}.`;
+}
+
 const countries = [
-  "japan",
-  "south africa",
-//   "nepal",
-//   "canada",
-//   "turkey",
-//   "new zealand",
-//   "mongolia",
-//   "india",
-//   "switzerland",
-//   "australia",
-//   "kenya",
-//   "jamaica",
-//   "iceland",
-//   "greece",
-//   "italy",
+  "Japan",
+  "South Africa",
+  "Nepal",
+  "Canada",
+  "Turkey",
+  "New Zealand",
+  "Mongolia",
+  "Panama",
+  "Switzerland",
+  "Australia",
+  "Kenya",
+  "Jamaica",
+  "Iceland",
+  "Greece",
+  "Italy",
 ];
 
 type CountryData = {
   name: { common: string };
   flags: { png: string };
+  area: number;
+  population: number;
+  continents: string[];
+  capital: string[];
 };
 
-const useAxios = () => {
-  const [data, setData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+const useAxios = (currentPage: number) => {
+  const [data, setData] = useState<CountryData[]>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+  const [area, setArea] = useState<number>();
+  const [population, setPopulation] = useState<number>();
+  const [continent, setContinent] = useState<string>("");
+  const [capital, setCapital] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
+  const [answerOptions, setAnswerOptions] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        for (const country of countries) {
-          const response = await axios.get<any[]>(
-            `https://restcountries.com/v3.1/name/${country}?fullText=true`
-          );
+        const response = await axios.get<CountryData[]>(
+          `https://restcountries.com/v3.1/name/${
+            countries[currentPage - 1]
+          }?fields=name,flags,capital,area,continents,population`
+        );
+        setData(response.data);
+        console.log(response?.data?.[0]);
 
-          setData((currentData) => [...currentData, response.data]);
+        const selectedCountry = response?.data?.[0]?.name?.common;
+        let options: string[] = [selectedCountry];
+        let filteredCountries = countries.filter(
+          (country) => country !== selectedCountry
+        );
+        for (let i = 0; i < 3; i++) {
+          const randomIndex = Math.floor(
+            Math.random() * filteredCountries.length
+          );
+          options.push(filteredCountries[randomIndex]);
+          // Remove o país escolhido para evitar duplicatas
+          filteredCountries.splice(randomIndex, 1);
         }
+        shuffleArray(options);
+        setAnswerOptions(options);
+        setArea(response?.data?.[0]?.area);
+        setPopulation(response?.data?.[0]?.population);
+        setContinent(response?.data?.[0]?.continents?.[0]);
+        setCapital(response?.data?.[0]?.capital?.[0]);
+        setCountry(selectedCountry);
       } catch (err) {
         setError("Erro ao carregar dados");
       } finally {
@@ -47,9 +103,21 @@ const useAxios = () => {
     };
 
     fetchData();
-  }, []);
+  }, [currentPage]);
 
-  return { data, loading, error };
+  return {
+    data,
+    options: answerOptions,
+    text: getText(
+      country,
+      area !== undefined ? area : 0,
+      population !== undefined ? population : 0,
+      capital,
+      continent
+    ),
+    loading,
+    error,
+  };
 };
 
 export default useAxios;
