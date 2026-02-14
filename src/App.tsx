7962545d151d72b12f16ab34e9fc87974dc14aa4
vitormoscoso <vitormoscoso@gmail.com>
2026-02-14 11:29:38 -0300

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import Game from "./pages/jogo/[id]";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/flag-game" element={<Home />} />
+        <Route path="/flag-game/jogo/:id" element={<Game />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
